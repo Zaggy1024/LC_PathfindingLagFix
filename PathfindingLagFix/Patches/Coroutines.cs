cf73250e527ec6c6148bc0562bfa59e797349e95
Zaggy1024 <Zaggy1024@gmail.com>
2024-01-10 18:10:12 -0600

    Check for paths inline in ChooseFarthestNodeFromPosition
    This allows us to find the first path that is accessible sinultaneously to checking for one without player line of sight, so that the fallback of returning just any accessible path is free.

--- PathfindingLagFix/Patches/Coroutines.cs
+++ PathfindingLagFix/Patches/Coroutines.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 using UnityEngine;
@@ -13,43 +13,55 @@ namespace PathfindingLagFix.Patches
 
             yield return null;
 
+            Transform firstAccessibleNode = null;
+
             var pathsRemaining = offset;
             for (var i = 0; i < candidateNodes.Length; i++)
             {
                 var transform = candidateNodes[i].transform;
-                if (!enemy.PathIsIntersectedByLineOfSight(transform.position, calculatePathDistance: false, avoidLineOfSight))
-                {
-                    enemy.mostOptimalDistance = Vector3.Distance(pos, transform.position);
-                    yield return transform;
-                    if (pathsRemaining == 0)
-                        yield break;
-                    pathsRemaining--;
-                }
-                else
+                var position = transform.position;
+
+                // Check if the path is accessible.
+                if (!enemy.agent.CalculatePath(position, enemy.path1))
                 {
                     yield return null;
+                    continue;
                 }
+                if (firstAccessibleNode == null)
+                    firstAccessibleNode = transform;
+
+                // Check if the end of the path is close enough to the target.
+                if (Vector3.Distance(enemy.path1.corners[enemy.path1.corners.Length - 1], RoundManager.Instance.GetNavMeshPosition(position, RoundManager.Instance.navHit, 2.7f)) > 1.5f)
+                {
+                    yield return null;
+                    continue;
+                }
+
+                if (avoidLineOfSight)
+                {
+                    // Check if any segment of the path enters a player's line of sight.
+                    bool pathObstructed = false;
+                    for (int segment = 1; segment < enemy.path1.corners.Length && !pathObstructed; segment++)
+                        pathObstructed = Physics.Linecast(enemy.path1.corners[segment - 1], enemy.path1.corners[segment], 0x40000);
+                    if (pathObstructed)
+                    {
+                        yield return null;
+                        continue;
+                    }
+                }
+
+                enemy.mostOptimalDistance = Vector3.Distance(pos, transform.position);
+                yield return transform;
+                if (pathsRemaining == 0)
+                    yield break;
+                pathsRemaining--;
             }
 
             // If all line of sight checks fail, we will find the furthest reachable path to allow the pathfinding to succeed once we set the target.
             // The vanilla version of this function may return an unreachable location, so the NavMeshAgent will reset it to a reachable location and
             // cause a delay in the enemy's movement.
             if (avoidLineOfSight)
-            {
-                Plugin.Instance.Logger.LogWarning("Failed to find a far location without LOS, resorting to furthest accessible path.");
-                foreach (var node in candidateNodes)
-                {
-                    if (!enemy.PathIsIntersectedByLineOfSight(node.transform.position, calculatePathDistance: false, avoidLineOfSight: false))
-                    {
-                        yield return node.transform;
-                        yield break;
-                    }
-                    else
-                    {
-                        yield return null;
-                    }
-                }
-            }
+                yield return firstAccessibleNode;
         }
     }
 }
