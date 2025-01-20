c4f61fa71ddb86377fff49a79cdb6dac850b6b40
Zaggy1024 <Zaggy1024@gmail.com>
2025-01-19 19:41:31 -0600

    Set the assembly version to match the mod version

--- PathfindingLagFix/Plugin.cs
+++ PathfindingLagFix/Plugin.cs
@@ -10,9 +10,9 @@ namespace PathfindingLagFix
     [BepInPlugin(MOD_UNIQUE_NAME, MOD_NAME, MOD_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private const string MOD_NAME = "PathfindingLagFix";
-        private const string MOD_UNIQUE_NAME = "Zaggy1024." + MOD_NAME;
-        private const string MOD_VERSION = "2.0.4";
+        internal const string MOD_NAME = "PathfindingLagFix";
+        internal const string MOD_UNIQUE_NAME = "Zaggy1024." + MOD_NAME;
+        internal const string MOD_VERSION = "2.0.4";
 
         private readonly Harmony harmony = new Harmony(MOD_UNIQUE_NAME);
 
