7d8ec33bf3fba3466de8d44fdfaa3b55397b05fa
Zaggy1024 <Zaggy1024@gmail.com>
2025-01-23 23:13:56 -0600

    Use a namespace declaration in Plugin.cs

--- PathfindingLagFix/Plugin.cs
+++ PathfindingLagFix/Plugin.cs
@@ -1,35 +1,34 @@
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 
 using PathfindingLagFix.Patches;
 using PathfindingLib;
 
-namespace PathfindingLagFix
+namespace PathfindingLagFix;
+
+[BepInPlugin(MOD_UNIQUE_NAME, MOD_NAME, MOD_VERSION)]
+[BepInDependency(PathfindingLibPlugin.PluginGUID, BepInDependency.DependencyFlags.HardDependency)]
+public class Plugin : BaseUnityPlugin
 {
-    [BepInPlugin(MOD_UNIQUE_NAME, MOD_NAME, MOD_VERSION)]
-    [BepInDependency(PathfindingLibPlugin.PluginGUID, BepInDependency.DependencyFlags.HardDependency)]
-    public class Plugin : BaseUnityPlugin
+    public const string MOD_NAME = "PathfindingLagFix";
+    public const string MOD_UNIQUE_NAME = "Zaggy1024." + MOD_NAME;
+    public const string MOD_VERSION = "2.0.6";
+
+    private readonly Harmony harmony = new(MOD_UNIQUE_NAME);
+
+    public static Plugin Instance { get; private set; }
+    public new ManualLogSource Logger => base.Logger;
+
+    public void Awake()
     {
-        public const string MOD_NAME = "PathfindingLagFix";
-        public const string MOD_UNIQUE_NAME = "Zaggy1024." + MOD_NAME;
-        public const string MOD_VERSION = "2.0.6";
+        Instance = this;
 
-        private readonly Harmony harmony = new(MOD_UNIQUE_NAME);
-
-        public static Plugin Instance { get; private set; }
-        public new ManualLogSource Logger => base.Logger;
-
-        public void Awake()
-        {
-            Instance = this;
-
-            harmony.PatchAll(typeof(PatchEnemyAI));
-            harmony.PatchAll(typeof(PatchFlowermanAI));
-            harmony.PatchAll(typeof(PatchCentipedeAI));
-            harmony.PatchAll(typeof(PatchPufferAI));
-            harmony.PatchAll(typeof(PatchDoublewingAI));
-            harmony.PatchAll(typeof(PatchFlowerSnakeEnemy));
-        }
+        harmony.PatchAll(typeof(PatchEnemyAI));
+        harmony.PatchAll(typeof(PatchFlowermanAI));
+        harmony.PatchAll(typeof(PatchCentipedeAI));
+        harmony.PatchAll(typeof(PatchPufferAI));
+        harmony.PatchAll(typeof(PatchDoublewingAI));
+        harmony.PatchAll(typeof(PatchFlowerSnakeEnemy));
     }
 }
